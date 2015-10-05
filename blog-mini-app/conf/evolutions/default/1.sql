0cae20817aa607e28a813d46e2283900dcefcaec
ferrolho <henriqueferrolho@gmail.com>
2015-10-05 23:20:01 +0100

    helped botas.....

--- blog-mini-app/conf/evolutions/default/1.sql
+++ blog-mini-app/conf/evolutions/default/1.sql
@@ -2,7 +2,7 @@
 
 # --- !Ups
 
-CREATE TABLE "Post" (
+CREATE TABLE "posts" (
     "id" bigint generate by default as IDENTITY (start with 1) NOT NULL PRIMARY KEY,
     "subject" varchar NOT NULL,
     "description" varchar NOT NULL
