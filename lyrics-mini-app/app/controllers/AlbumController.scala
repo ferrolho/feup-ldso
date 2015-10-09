6cd813c61dc72348d721019e702232c67fa03985
ferrolho <henriqueferrolho@gmail.com>
2015-10-09 19:04:04 +0100

    solved problem mapping multiple values

--- lyrics-mini-app/app/controllers/AlbumController.scala
+++ lyrics-mini-app/app/controllers/AlbumController.scala
@@ -14,25 +14,18 @@ import scala.concurrent.{Future, ExecutionContext}
 class AlbumController @Inject()(repo: AlbumRepository, musicRepo: MusicRepository, val messagesApi: MessagesApi)
                                (implicit ec: ExecutionContext) extends Controller with I18nSupport {
 
-  //  def index = Action {
-  //    Ok(views.html.albums.index(albumForm, repo.list()))
-  //  }
-
-  // TODO - Does this really need to be async and use a map? Can't we use the above method instead?
   def index = Action.async {
-    repo.all.map { albums =>
+    repo.all map { albums =>
       Ok(views.html.albums.index(albumForm, albums))
     }
   }
 
   def show(id: Long) = Action.async {
-    musicRepo.fromAlbum(id).map { musics =>
-      Ok(views.html.albums.show(musics))
+    musicRepo.fromAlbum(id) flatMap { musics =>
+      repo.lookup(id) map { album =>
+        Ok(views.html.albums.show(album, musics))
+      }
     }
-
-//    repo.lookup(id).map { album =>
-//      Ok(views.html.albums.show(album, Nil))
-//    }
   }
 
   val albumForm: Form[CreateAlbumForm] = Form {
