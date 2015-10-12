40a2b5d623c5b118be456bdf30a0b2f8b50397bd
ferrolho <henriqueferrolho@gmail.com>
2015-10-13 00:40:11 +0100

    Added silhouette module :) login and sign up done!

--- project-app/app/controllers/Application.scala
+++ project-app/app/controllers/Application.scala
@@ -1,15 +1,66 @@
 package controllers
 
+import javax.inject.Inject
+
+import com.mohiva.play.silhouette.api.{LogoutEvent, Silhouette, Environment}
+import com.mohiva.play.silhouette.impl.authenticators.CookieAuthenticator
+import com.mohiva.play.silhouette.impl.providers.SocialProviderRegistry
+import forms._
+import models.User
+import play.api.i18n.MessagesApi
 import play.api.mvc._
 
-class Application extends Controller {
+import scala.concurrent.Future
 
-  def index = Action {
-    Ok(views.html.index())
+class Application @Inject()(
+                             val messagesApi: MessagesApi,
+                             val env: Environment[User, CookieAuthenticator],
+                             socialProviderRegistry: SocialProviderRegistry)
+  extends Silhouette[User, CookieAuthenticator] {
+
+  /**
+   * Handles the index action.
+   *
+   * @return The result to display.
+   */
+  def index = SecuredAction.async { implicit request =>
+    Future.successful(Ok(views.html.home(request.identity)))
   }
 
-  def dashboard = Action {
-    Ok(views.html.dashboard())
+  /**
+   * Handles the Sign In action.
+   *
+   * @return The result to display.
+   */
+  def signIn = UserAwareAction.async { implicit request =>
+    request.identity match {
+      case Some(user) => Future.successful(Redirect(routes.Application.index()))
+      case None => Future.successful(Ok(views.html.auth.signIn(SignInForm.form, socialProviderRegistry)))
+    }
+  }
+
+  /**
+   * Handles the Sign Up action.
+   *
+   * @return The result to display.
+   */
+  def signUp = UserAwareAction.async { implicit request =>
+    request.identity match {
+      case Some(user) => Future.successful(Redirect(routes.Application.index()))
+      case None => Future.successful(Ok(views.html.auth.signUp(SignUpForm.form)))
+    }
+  }
+
+  /**
+   * Handles the Sign Out action.
+   *
+   * @return The result to display.
+   */
+  def signOut = SecuredAction.async { implicit request =>
+    val result = Redirect(routes.Application.index())
+    env.eventBus.publish(LogoutEvent(request.identity, request, request2Messages))
+
+    env.authenticatorService.discard(request.authenticator, result)
   }
 
   def playInfo = Action {
--- project-app/build.sbt
+++ project-app/build.sbt
@@ -4,21 +4,26 @@ version := "1.0-SNAPSHOT"
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
-scalaVersion := "2.11.6"
+scalaVersion := "2.11.7"
+
+resolvers += "Atlassian Releases" at "https://maven.atlassian.com/public/"
+resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
 
 libraryDependencies ++= Seq(
+  "com.mohiva" %% "play-silhouette" % "3.0.4",
+  "com.mohiva" %% "play-silhouette-testkit" % "3.0.4" % "test",
+  "org.webjars" %% "webjars-play" % "2.4.0-1",
+  "net.codingwell" %% "scala-guice" % "4.0.0",
+  "net.ceedubs" %% "ficus" % "1.1.2",
+  "com.adrianhurt" %% "play-bootstrap3" % "0.4.4-P24",
+  specs2 % Test,
   "com.typesafe.play" %% "play-slick" % "1.0.1",
   "com.typesafe.play" %% "play-slick-evolutions" % "1.0.1",
   "com.h2database" % "h2" % "1.4.189",
-  "com.mohiva" %% "play-silhouette" % "3.0.4",
-  "com.mohiva" %% "play-silhouette-testkit" % "3.0.4" % "test",
   cache,
-  specs2 % Test,
-  ws
+  ws,
+  filters
 )
-
-resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
-resolvers += "Atlassian Releases" at "https://maven.atlassian.com/public/"
 
 // Play provides two styles of routers, one expects its actions to be injected, the
 // other, legacy style, accesses its actions statically.
