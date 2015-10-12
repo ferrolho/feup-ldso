cde3490c64e4118a9bdfb2514426f21f1788313e
ferrolho <henriqueferrolho@gmail.com>
2015-10-12 17:20:23 +0100

    starting Iteration 0 US

--- project-app/app/controllers/Application.scala
+++ project-app/app/controllers/Application.scala
@@ -5,7 +5,15 @@ import play.api.mvc._
 class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index())
+  }
+
+  def dashboard = Action {
+    Ok(views.html.dashboard())
+  }
+
+  def playInfo = Action {
+    Ok(views.html.playInfo("Your application is ready."))
   }
 
 }
