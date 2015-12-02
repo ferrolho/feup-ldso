b68dd349b39d73ef68595a63903dd0a76360ac2a
lycantropus <mechanical.maggot.0@gmail.com>
2015-12-02 23:49:19 +0000

    front end things

--- project-app/app/controllers/TransportController.scala
+++ project-app/app/controllers/TransportController.scala
@@ -4,7 +4,7 @@ import javax.inject.Inject
 
 import com.mohiva.play.silhouette.api.{Environment, Silhouette}
 import com.mohiva.play.silhouette.impl.authenticators.CookieAuthenticator
-import models.{SortingCenterStock, User}
+import models.{SortingCenterStock, User, Transport}
 import play.api.i18n.MessagesApi
 import models.services.{ResourceAmountLabelService, ResourceCategoryService, SortingCenterStockService, TransportService}
 import models.daos.UserDAO
@@ -33,10 +33,13 @@ class TransportController @Inject()(
     var sourceEmailsList = new ListBuffer[String]()
     var destinyEmailsList = new ListBuffer[String]()
     var sortingCenterStockList = new ListBuffer[SortingCenterStock]()
+    val transportsList = new ListBuffer[Transport]()
 
     // TODO ask Sereno for a better way to do this
     transportService.allNonActive().map { transports =>
       transports.map { transport =>
+
+        transportsList += transport
 
         // gets email from source
         userDAO.find(transport.idSourceUser).map { option =>
@@ -61,6 +64,6 @@ class TransportController @Inject()(
       }
     }
 
-      Future.successful(Ok(views.html.transports.index(request.identity, sourceEmailsList.toList, destinyEmailsList.toList, sortingCenterStockList.toList)))
+      Future.successful(Ok(views.html.transports.index(request.identity, sourceEmailsList.toList, destinyEmailsList.toList, sortingCenterStockList.toList, transportsList.toList)))
   }
 }
