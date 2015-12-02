aa9cfc3fe2f8ed8ead042e72f19b5138eb7dbcee
rafikii <rafaelapintofaria@gmail.com>
2015-12-02 20:32:00 +0100

    now it's ok

--- project-app/app/controllers/RequestsController.scala
+++ project-app/app/controllers/RequestsController.scala
@@ -31,19 +31,21 @@ class RequestsController @Inject()(
 
   def index = SecuredAction.async { implicit request =>
 
-    val fAllSupliesExceptUser = supplyService.allExceptByUser(request.identity.userID)
+    val fAllSortingCenterStockUser = sortingCenterStockService.byUser(request.identity.userID)
     val fResourceCategories = resourceCategoryService.all
     val fResourceAmountLabels = resourceAmountLabelService.all
 
     for {
-      allSuppliesExceptUser <- fAllSupliesExceptUser; resourceCategories <- fResourceCategories; resourceAmountLabels <- fResourceAmountLabels
+      allSortingCenterStocks <- fAllSortingCenterStockUser
+      resourceCategories <- fResourceCategories;
+      resourceAmountLabels <- fResourceAmountLabels
     }
       yield {
-        val supplies = allSuppliesExceptUser
+        val sortingCenterStocks = allSortingCenterStocks
         val resourceCategoryOptions = resourceCategories.map { model => (model.id.toString, model.name) }
         val resourceAmountLabelOptions = resourceAmountLabels.map { model => (model.id.toString, model.name) }
 
-        Ok(views.html.requests.index(request.identity, SortingCenterStockForm.form, supplies, resourceCategoryOptions, resourceAmountLabelOptions))
+        Ok(views.html.requests.index(request.identity, sortingCenterStocks, resourceCategoryOptions, resourceAmountLabelOptions))
       }
   }
 
@@ -52,6 +54,7 @@ class RequestsController @Inject()(
    *
    * @return The result to display.
    */
+  /*
   def acceptOffer = SecuredAction.async { implicit request =>
     SortingCenterStockForm.form.bindFromRequest.fold(
       form => {
@@ -65,7 +68,7 @@ class RequestsController @Inject()(
             val resourceCategoryOptions = resourceCategories.map { model => (model.id.toString, model.name) }
             val resourceAmountLabelOptions = resourceAmountLabels.map { model => (model.id.toString, model.name) }
 
-            BadRequest(views.html.requests.index(request.identity, form, supplies, resourceCategoryOptions, resourceAmountLabelOptions))
+            BadRequest(views.html.sortingCenters.index(request.identity, form, supplies, resourceCategoryOptions, resourceAmountLabelOptions))
           }
       },
       data => {
@@ -125,41 +128,32 @@ class RequestsController @Inject()(
       }
     )
   }
+  */
 
   /**
-   * Cancels a previous sorting center stock request and updates the avaiable supllies
+   * Gets all session's user sorting center stocks
    *
    * @return The result to display
    */
-  def cancelIncomingResource(sortingCenterStockID: String) = SecuredAction.async { implicit request =>
-    sortingCenterStockService.retrieve(UUID.fromString(sortingCenterStockID)).flatMap { stock =>
-      supplyService.retrieve(stock.idSupply).map {
-        case Some(supply) =>
-          supplyService.save(Supply(
-            supply.id,
-            supply.userID,
-            supply.resource,
-            supply.resourceCategoryID,
-            amount = supply.amount + stock.amount,
-            supply.amountLabelID
-          ))
+  /*
+  def incomingResources = SecuredAction.async { implicit request =>
 
-          sortingCenterStockService.delete(stock.id)
+    val fAllSortingCenterStockUser = sortingCenterStockService.byUser(request.identity.userID)
+    val fResourceCategories = resourceCategoryService.all
+    val fResourceAmountLabels = resourceAmountLabelService.all
 
-        case None =>
-          supplyService.save(Supply(
-            stock.idSupply,
-            stock.supplyUserID,
-            stock.resource,
-            stock.resourceCategoryID,
-            stock.amount,
-            stock.amountLabelID
-          ))
-
-          sortingCenterStockService.delete(stock.id)
-      }
-
-      Future.successful(Redirect(routes.SortingCentersController.incomingResources))
+    for {
+      allSortingCenterStocks <- fAllSortingCenterStockUser
+      resourceCategories <- fResourceCategories;
+      resourceAmountLabels <- fResourceAmountLabels
     }
+      yield {
+        val sortingCenterStocks = allSortingCenterStocks
+        val resourceCategoryOptions = resourceCategories.map { model => (model.id.toString, model.name) }
+        val resourceAmountLabelOptions = resourceAmountLabels.map { model => (model.id.toString, model.name) }
+
+        Ok(views.html.request.index(request.identity, sortingCenterStocks, resourceCategoryOptions, resourceAmountLabelOptions))
+      }
   }
+  */
 }
