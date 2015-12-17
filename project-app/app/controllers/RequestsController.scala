e45b8334618b2f6821c36b6b6d6b2c3aee2246e7
rafikii <rafaelapintofaria@gmail.com>
2015-12-17 01:28:54 +0100

    Starting US

--- project-app/app/controllers/RequestsController.scala
+++ project-app/app/controllers/RequestsController.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import java.util.UUID
 import javax.inject.Inject
 
 import com.mohiva.play.silhouette.api.{Environment, Silhouette}
@@ -43,6 +44,43 @@ class RequestsController @Inject()(
 
       Ok(views.html.requests.index(request.identity, sortingCenterStocks, resourceCategoryOptions, resourceAmountLabelOptions))
     }
+  }
+
+  /**
+   * Submits a request.
+   *
+   * @return The result to display.
+   */
+  def submitSupplyOffer = SecuredAction.async { implicit request =>
+    RequestForm.form.bindFromRequest.fold(
+      form => {
+        val fSupplies = supplyService.byUser(request.identity.userID)
+        val fResourceCategories = resourceCategoryService.all
+        val fResourceAmountLabels = resourceAmountLabelService.all
+
+        for {supplies <- fSupplies; resourceCategories <- fResourceCategories; resourceAmountLabels <- fResourceAmountLabels}
+          yield {
+            val resourceCategorySelectOptions = resourceCategories.map { model => (model.id.toString, model.name) }
+            val resourceAmountLabelSelectOptions = resourceAmountLabels.map { model => (model.id.toString, model.name) }
+
+            BadRequest(views.html.supplies.index(request.identity, form, supplies, resourceCategorySelectOptions, resourceAmountLabelSelectOptions))
+          }
+      },
+      data => {
+        val supply = Request(
+          id = UUID.randomUUID(),
+          userID = request.identity.userID,
+          resource = data.resource,
+          resourceCategoryID = data.resourceCategoryID,
+          amount = data.amount,
+          amountLabelID = data.amountLabelID
+        )
+
+        for {
+          supply <- supplyService.save(supply.copy())
+        } yield Redirect(routes.SuppliesController.index())
+      }
+    )
   }
 
 }
--- project-app/app/forms/RequestForm.scala
+++ project-app/app/forms/RequestForm.scala
@@ -3,8 +3,14 @@ package forms
 import play.api.data.Form
 import play.api.data.Forms._
 
+/**
+ * The form which handles the submission of a request.
+ */
 object RequestForm {
 
+  /**
+   * A play framework form.
+   */
   val form = Form(
     mapping(
       "supplyID" -> nonEmptyText,
@@ -12,6 +18,12 @@ object RequestForm {
     )(Data.apply)(Data.unapply)
   )
 
+  /**
+   * The form data.
+   *
+   * @param supplyID The name of the resource.
+   * @param amount The amount of resource units being supplied.
+   */
   case class Data(supplyID: String, amount: Int)
 
 }
