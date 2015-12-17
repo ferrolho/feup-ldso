80dbc7c0b67e90b7697be0d05e59331273faf1cc
ferrolho <henriqueferrolho@gmail.com>
2015-12-16 21:12:46 -0500

    changes to redirect html and deploy script

--- project-app/deploy.sh
+++ project-app/deploy.sh
@@ -1,3 +1,6 @@
+# kill previous running app (if any)
+kill $(cat target/universal/project-app-1.0-SNAPSHOT/RUNNING_PID)
+
 git pull
 
 ./activator dist
@@ -7,4 +10,4 @@ rm -r project-app-1.0-SNAPSHOT
 unzip project-app-1.0-SNAPSHOT.zip
 cd project-app-1.0-SNAPSHOT/bin/
 
-./project-app -Dhttp.port=8080 -Dplay.evolutions.db.default.autoApply=true -Dplay.crypto.secret="Hcey3kRQ@?=_S__2H7pvQ@mkxJ8kF`KAe7X<<K3:56@X^B>m/Owzevh<C@D`be/J" &
+./project-app -Dplay.evolutions.db.default.autoApply=true -Dplay.crypto.secret="Hcey3kRQ@?=_S__2H7pvQ@mkxJ8kF`KAe7X<<K3:56@X^B>m/Owzevh<C@D`be/J" &
